b9cb9a3f7f8eda2837336d31e3066846e660aa2d
Toshiaki Asai <qtoship@gmail.com>
2012-03-17 01:32:09 +0900

    フィルタの設定変更を再起動無しでできるようにした

--- filter.rb
+++ filter.rb
@@ -1,38 +1,35 @@
 #!/usr/bin/env ruby -Ku
 # -*- coding: utf-8 -*-
-Plugin.create(:cilent_filter) do
-  exclude_client = UserConfig[:filter_mute_kind_client]
-  if exclude_client.is_a? Array then
-    filter_show_filter do |msgs|
+Plugin.create(:filter) do
+  filter_show_filter do |msgs|
+    mute_words = UserConfig[:filter_mute_kind_client].select{|m|!m.empty?}
+    if mute_words
       msgs = msgs.select{ |m|
-        not exclude_client.any?{ |word|
+        not UserConfig[:filter_mute_kind_client].any?{ |word|
           word.to_s.include?(m[:source]) if m[:source] != nil
         }
       }
-      [msgs]
     end
+    [msgs]
   end
-end
 
-Plugin.create(:mute_word) do
-  exclude_word = UserConfig[:filter_mute_word]
-  if exclude_word.is_a? Array then
-    filter_show_filter do |msgs|
+  filter_show_filter do |msgs|
+    mute_words = UserConfig[:filter_mute_word].select{|m|!m.empty?}
+    if mute_words
       msgs = msgs.select{ |m|
-        not exclude_word.any?{ |word| 
+        not mute_words.any?{ |word|
           m.to_s.include?(word)
-        } 
+        }
       }
-      [msgs]
     end
+    [msgs]
   end
-end
 
-Plugin.create :filter do
   settings "ミュート" do
     settings "フィルタする「種類」\n※設定は再起動後に有効になります" do
       multi "クライアント", :filter_mute_kind_client
       multi "単語", :filter_mute_word
     end
   end
+
 end
